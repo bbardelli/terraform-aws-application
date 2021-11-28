1fedfe854d6ae16bee8cf47b75faa64440839a4c
Bruno Augusto Bardelli <brunobardelli@gmail.com>
2021-11-28 22:32:10 +0100

    testing asg

---
+++ asg.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "app"{
+    most_recent = true
+    owners = ["self"]
+
+    filter {
+        name   = "name"
+        values = [var.ami_name]
+    }
+}
+
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix   = "terraform-lc-application-"
+  image_id      = data.aws_ami.app.id
+  instance_type = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.application_profile.id
+  user_data = <<EOF
+        #!/bin/bash
+        sudo echo '${var.db_endpoint}' >> /var/application/config.db        
+        EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "application_asg" {  
+  desired_capacity   = 2
+  max_size           = 2
+  min_size           = 1
+  launch_configuration = aws_launch_configuration.as_conf.name
+  aws_lb_target_group = aws_lb_target_group.application.id
+}
---
+++ lb.tf
@@ -0,0 +1,27 @@
+resource "aws_lb" "application" {
+  name               = "application-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.application_access.id]  
+
+  enable_deletion_protection = false
+  
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "8080"
+  protocol          = "HTTP"  
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.application.arn
+  }
+}
+
+resource "aws_lb_target_group" "application" {
+  name     = "tf-application-lb-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
